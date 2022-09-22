4331d435ad67f4a62b1bb11116781fef0ebba89b
Emmanuel Hateka <emmanuelhateka.com>
2022-09-22 10:29:15 +0000

    Skills and Testimonials section in progress

--- client/js/app.js
+++ client/js/app.js
@@ -12,4 +12,33 @@
   };
 
   hamburgerMenu.addEventListener("click", mobileMenuToggler);
+
+  // Testimonials Slider
+  const slide = document.getElementById("slide");
+  const arrowUp = document.getElementById("arrowUp");
+  const arrowDown = document.getElementById("arrowDown");
+  const testimonial = document.querySelectorAll(".testimonial");
+
+  let counter = 0;
+  let size = testimonial[0].clientHeight + 2;
+  const testimonialLength = testimonial.length - 1;
+
+  const testimonialSlideDown = () => {
+    if (counter < size * testimonialLength) {
+      slide.style.top = `-${size + counter}px`;
+      counter += size;
+    }
+  };
+
+  console.log(counter);
+  arrowDown.addEventListener("click", testimonialSlideDown);
+
+  const testimonialSlideUp = () => {
+    if (counter >= size) {
+      slide.style.top = `-${counter - size}px`;
+      counter -= size;
+    }
+  };
+
+  arrowUp.addEventListener("click", testimonialSlideUp);
 })();
